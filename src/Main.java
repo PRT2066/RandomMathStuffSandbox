2b20c055f834eac2df085ca8f71f0e3d604d96f0
Paul Tegen <prt2066@gmail.com>
2025-03-07 01:58:45 -0600

    master cleanup of calculateNumbers method. condense into switch-case statement

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
         CalculateStuff calculate = new CalculateStuff();
 
--- src/Stuff/CalculateStuff.java
+++ src/Stuff/CalculateStuff.java
@@ -10,38 +10,33 @@ public class CalculateStuff {
     private BigDecimal num2 = BigDecimal.ZERO;
 
 
-    public BigDecimal calculateNumbers(BigDecimal num1, BigDecimal num2, OperationModeEnums operator){
-        if(num1 == null || num2 == null){
+    public BigDecimal calculateNumbers(BigDecimal num1, BigDecimal num2, OperationModeEnums operator) {
+        if (num1 == null || num2 == null) {
             System.out.println("One of the numbers you gave me is null. Cannot calculate.");
             return null;
         }
 
-        if(operator.equals(OperationModeEnums.ADDITION)){
-            return num1.add(num2);
-        }
-        else if(operator.equals(OperationModeEnums.SUBTRACTION)){
-            return num1.subtract(num2);
-        }
-        else if(operator.equals(OperationModeEnums.MULTIPLY)){
-            return num1.multiply(num2);
-        }
-        else if(operator.equals(OperationModeEnums.DIVISION)){
-            if(!num1.equals(BigDecimal.ZERO) && !num2.equals(BigDecimal.ZERO)) {
-                // If the number divides cleanly then we don't need to output with scale and/or rounding.
-                // Otherwise, while it's not exactly precise...I'm deciding to limit scale to 3 places.
+        switch (operator) {
+            case ADDITION:
+                return num1.add(num2);
+            case SUBTRACTION:
+                return num1.subtract(num2);
+            case MULTIPLY:
+                return num1.multiply(num2);
+            case DIVISION:
+                if (num1.equals(BigDecimal.ZERO) || num2.equals(BigDecimal.ZERO)) {
+                    System.out.println("Cannot divide by zero.");
+                    return null;
+                }
                 try {
                     return num1.divide(num2, RoundingMode.UNNECESSARY);
                 } catch (ArithmeticException e) {
                     return num1.divide(num2, 3, RoundingMode.HALF_UP);
                 }
-            }
-            else {
+            default:
+                System.out.println("Unsupported operation.");
                 return null;
-            }
         }
-
-
-        return null;
     }
 
     public BigDecimal getNum1() {
